e3bb5b662e36f888b48bcd0acaa1d13faff7cc80
kranthitech <kranthitech@gmail.com>
2015-09-18 12:15:55 +0530

    Made a boiler plate from generator-gulp-angular 1. Using Angular Material 2. Ui Router

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('boilerplate', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ui.router', 'ngMaterial','angular-loading-bar']);
+
+})();
